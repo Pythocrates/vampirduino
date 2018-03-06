969bd743704e5367d3ed37a41ca3c8b92620b994
Martin Krzykawski <fun@martin-krzykawski.de>
2018-03-06 21:25:57 +0100

    Working version. Suppressing early cat interrupts.

--- diorama_control/diorama_control.ino
+++ diorama_control/diorama_control.ino
@@ -2,13 +2,25 @@
  * Vampire diorama control project.
  * Detect a cat using distance sensor with digital output.
  * Actuate one door.
- * Actuate one person?
- * Actuate one garage door?
- * Switch on/off light?
+ * Actuate one person.
+ * Switch on/off light.
  * Use timers to switch off stuff.
  * Use interrupt to react to cat detection.
- * Play a melody with a piezo?
  */
+
+class Door {
+public:
+    class Motion {
+    public:
+        typedef int Type;
+        static const Type NONE = 0;
+        static const Type CW = 1 << 0;
+        static const Type CCW = 1 << 1;
+    };
+
+    int motion = Motion::NONE;
+};
+
 
 #include <Stepper.h>
 
@@ -27,18 +39,22 @@ int door_direction = 1;
 int cat_detector_pin = 2;
 int red_light_pin = 8;
 
-
 void setup() {
     // Set up pins.
     pinMode(LED_BUILTIN, OUTPUT);
-
     pinMode(cat_detector_pin, INPUT); 
     pinMode(red_light_pin, OUTPUT);
     
     // The detection means output going low.
+
+    Serial.begin(9600);
+    Serial.println("Delay...");
+    delay(2000);
+    Serial.println("... done!");
+    noInterrupts();
+    EIFR |= (1 << INTF0); // Clear pending interrupts.
     attachInterrupt(digitalPinToInterrupt(cat_detector_pin), on_cat_detected, FALLING);
 
-    noInterrupts();
     TCCR1A = 0; // clear registers
     TCCR1B = 0;
     TCNT1 = 0;
@@ -49,12 +65,11 @@ void setup() {
     //TIMSK1 = bit(OCIE1A); // Set interrupt
     interrupts();
 
-    Serial.begin(9600);
     myStepper.setSpeed(400);
 }
 
 ISR(TIMER1_COMPA_vect) {
-        Serial.println("ISR");
+    Serial.println("ISR");
     digitalWrite(LED_BUILTIN, LOW);
     TIMSK1 = 0; // Disable interrupt
     door_moving = true;
@@ -80,29 +95,28 @@ void loop() {
             door_direction = -door_direction;
             stepCount = 0;
 
-if (door_direction < 0) {
-// Start timer.
-Serial.println("Starting timer");
-    noInterrupts();
-    TIMSK1 = 0;
-    TCNT1 = 0;
-    TCCR1A = 0;
-    //TCCR1B = 0;
-TCCR1B &= ~(1 << CS10);
-TCCR1B &= ~(1 << CS12);
+            if (door_direction < 0) {
+                // Start timer.
+                Serial.println("Starting timer");
+                noInterrupts();
+                TIMSK1 = 0;
+                TCNT1 = 0;
+                TCCR1A = 0;
+                //TCCR1B = 0;
+                TCCR1B &= ~(1 << CS10);
+                TCCR1B &= ~(1 << CS12);
 
-TIFR1 = bit (OCF2A); // clear any pending interrupt: https://arduino.stackexchange.com/questions/22432/irregularly-triggering-an-isr-using-timers
+                TIFR1 = bit (OCF2A); // clear any pending interrupt: https://arduino.stackexchange.com/questions/22432/irregularly-triggering-an-isr-using-timers
 
-    TCCR1B = bit(WGM12) | bit(CS10) | bit(CS12); // prescaler 1024 and CTC mode
-    OCR1A = 31250; // set 0.5sec timer
-    Serial.println(TCNT1);
-    TIMSK1 = bit(OCIE1A); // Set interrupt
-    interrupts();
-} else {
-  digitalWrite(red_light_pin, LOW);
-  }
+                TCCR1B = bit(WGM12) | bit(CS10) | bit(CS12); // prescaler 1024 and CTC mode
+                OCR1A = 31250; // set 0.5sec timer
+                Serial.println(TCNT1);
+                TIMSK1 = bit(OCIE1A); // Set interrupt
+                interrupts();
+            } else {
+                digitalWrite(red_light_pin, LOW);
+            }
         }
-        
     }
     
     delay(20);
